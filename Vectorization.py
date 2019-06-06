7e8494204c41bacc0ca4ce7aaf811e77107889ef
lind1022 <lukeintotherain@msn.com>
2019-06-06 22:14:22 +1200

    second commit

--- Vectorization.py
+++ Vectorization.py
@@ -1,4 +1,23 @@
 import numpy as np
+import time
 
-a = np.array([1, 2, 3, 4])
-print(a)
+# Vectorized version
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a, b)
+toc = time.time()
+
+print("Vectorized version:" + str(10000*(toc-tic)) + "ms")
+
+# For loop version
+c = 0
+tic = time.time()
+for i in range(1000000):
+    c += a[i]*b[i]
+toc = time.time()
+
+print(c)
+print("For loop:" + str(10000*(toc-tic)) + "ms")
