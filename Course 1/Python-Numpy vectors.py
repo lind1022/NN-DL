de1e694b40471d7f977779657d2ba88cb6a72138
lind1022 <lukeintotherain@msn.com>
2019-06-13 17:43:23 +1200

    2nd

---
+++ Course 1/Python-Numpy vectors.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.random.randn(5)  # Creates a rank 1 array
+
+print(np.dot(a, a.T)) # expecting an outer product, but got a number
+
+a = np.random.randn(5, 1)
+print(a)
+print(a.T)
+
+print(np.dot(a, a.T))
